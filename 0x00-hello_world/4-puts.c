3d30e285d96896eeca662e12483f16da5ea31740
Alae Bouhmala <alaebouhmala@outlook.com>
2023-03-16 11:19:16 -0700

    Betty coding style fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a Text 
- *
- * Return: Always 0 (Success)
- */
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return 0;
-
+	return (0);
 }
